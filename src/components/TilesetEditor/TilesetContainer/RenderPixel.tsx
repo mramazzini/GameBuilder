66aeff24340b30847347e5218fd0f1768da46c60
mramazzini <mramazzini123@gmail.com>
2023-11-27 14:47:21 -0600

    Added placeholder texture for no color found

--- src/components/TilesetEditor/TilesetContainer/RenderPixel.tsx
+++ src/components/TilesetEditor/TilesetContainer/RenderPixel.tsx
@@ -13,13 +13,33 @@ const RenderPixel = ({ pixel, row, col }: RenderTileProps) => {
     }
     return `none`;
   };
+  const calculateColor = () => {
+    if (pixel[3] !== 0) {
+      return `rgba(${pixel[0]}, ${pixel[1]}, ${pixel[2]}, ${pixel[3]})`;
+    }
+    //return grey or white depending on row and col
+    if (row % 2 === 0 && col % 2 === 0) {
+      return `rgba(${100}, ${100}, ${100}, ${255})`;
+    }
+    if (row % 2 === 0 && col % 2 !== 0) {
+      return `rgba(${255}, ${255}, ${255}, ${255})`;
+    }
+    if (row % 2 !== 0 && col % 2 === 0) {
+      return `rgba(${255}, ${255}, ${255}, ${255})`;
+    }
+    if (row % 2 !== 0 && col % 2 !== 0) {
+      return `rgba(${100}, ${100}, ${100}, ${255})`;
+    }
+
+    return `rgba(${0}, ${0}, ${0}, ${255})`;
+  };
 
   return (
     <div
       key={`tile-${row}-${col}`}
       className='tile'
       style={{
-        backgroundColor: `rgba(${pixel[0]}, ${pixel[1]}, ${pixel[2]}, ${pixel[3]})`,
+        backgroundColor: calculateColor(),
         width: `1px`,
         height: `1px`,
       }}
